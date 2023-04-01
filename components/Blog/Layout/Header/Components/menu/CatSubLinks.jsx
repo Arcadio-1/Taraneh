7a5915dbb914a759510004b170e69a980229c8d2
arcadio <c4net.7@gmail.com>
2023-04-01 04:45:51 +0330

    fix bug

--- components/Blog/Layout/Header/Components/menu/CatSubLinks.jsx
+++ components/Blog/Layout/Header/Components/menu/CatSubLinks.jsx
@@ -1,6 +1,5 @@
 import Link from "next/link";
 import React from "react";
-// import { Link } from "react-router-dom";
 import NewsIcon from "../../../../../ui/Icons/NewsIcon";
 import CoffeeIcon from "../../../../../ui/Icons/CoffeeIcon";
 import DrinkIcon from "../../../../../ui/Icons/DrinkIcon";
