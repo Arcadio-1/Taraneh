42da93291bb2e04f0f221dce20ee9086dfbc774f
arcadio <c4net.7@gmail.com>
2023-07-31 21:37:42 +0330

    change databse

--- pages/api/util/functions/getClient.js
+++ pages/api/util/functions/getClient.js
@@ -3,7 +3,7 @@ import { MongoClient } from "mongodb";
 export const getClient = async (databaseName) => {
   try {
     const client = await MongoClient.connect(
-      `${process.env.REACT_APP_MONGODB_2}${databaseName}?retryWrites=true&w=majority`
+      `${process.env.REACT_APP_MONGODB}${databaseName}?retryWrites=true&w=majority`
     );
     return client;
   } catch (error) {
